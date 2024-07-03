35c311d57b14defe0946355227a7ffdf8441ad51
Evan0707 <64790334+Evan0707@users.noreply.github.com>
2024-07-03 10:51:50 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(express.json({
     limit:'10mb'
 }))
 app.use(cors({
-    origin:["https://vibes-client.vercel.app"],
+    origin:["https://vibes-cli.vercel.app"],
     methods:["POST","GET"],
     credentials:true
 }))
